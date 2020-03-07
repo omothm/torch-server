89697b7ad1c572e35c5050bfae6dd78027aeeab0
Omar Othman <omar@omothm.com>
2020-03-07 20:38:07 +0300

    :wrench: add post test

--- test/test.py
+++ test/test.py
@@ -1,29 +1,41 @@
 """Torch Server test
+
+Sends GET and POST requests to the server.
 """
 
 __author__ = "Omar Othman <omar@omothm.com>"
 
 
 import os
-import json
-import requests
 import urllib.parse
+import requests
+
 
 _CURRENT_DIR = os.path.dirname(__file__)
 _URL = "http://localhost:8000/api/"
 
 
 def main():
-    """Sends a GET request to the server and prints the response
+    """Sends GET and POST requests to the server and prints the response
     """
 
     # get an example base-64 encoded image from an input file
-    with open(os.path.join(_CURRENT_DIR, "input\\base64_example.txt")) as b64file:
+    with open(os.path.join(_CURRENT_DIR, "input", "base64_example.txt")) as b64file:
         example_base64 = b64file.read()
 
-    # send a GET request to the server
-    res = requests.get(url=_URL, params={"service": "banknote","image":urllib.parse.quote_plus(example_base64)})
+    service = "banknote"
 
+    print("Sending a GET request...")
+    res = requests.get(url=_URL, params={
+        "service": service,
+        "image": urllib.parse.quote_plus(example_base64)
+    })
+    print(res.text)
+
+    print()
+    print("Sending a POST request...")
+    url = f"{_URL}?service={service}"
+    res = requests.post(url=url, data=example_base64)
     print(res.text)
 
 
