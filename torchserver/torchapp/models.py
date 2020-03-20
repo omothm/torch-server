320841c1210d9d07347c29358d39d0805ac7d02a
Ezgi Nur Ucay <ezginrucay@gmail.com>
2020-03-20 14:43:21 +0300

    :art: model and serializers files deleted.

--- torchserver/torchapp/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class Banknote(models.Model):
-    image_base64 = models.TextField(blank=False)
--- torchserver/torchapp/urls.py
+++ torchserver/torchapp/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.api_get_request),
-    #path('banknote/<int:pk>/', views.set_banknote),
 ]
