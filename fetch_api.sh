f87073ce12d68db14260f66cb05b5fb673377ab4
Emre Biçer <emreee.bicer@gmail.com>
2020-05-13 23:05:58 +0300

    :zap: update fetch_api.sh
    Avoid compiling object detection module again and again

--- fetch_api.sh
+++ fetch_api.sh
@@ -5,7 +5,6 @@ function cleanup() {
 
         echo "Cleaning up.."
         rm -rf $tempdir
-        rm -rf $tensorflow_dir
         echo "Done."
         exit 0
 }
@@ -39,17 +38,17 @@ tensorflow_dir="models"
 if [[ ! -d "${tensorflow_dir}" && ! -L "${tensorflow_dir}" ]] ; then
         echo "Can't find tensorflow files, fetching them"
         git clone --depth 1 https://github.com/tensorflow/models/
+        pip install cython
+        pip install pycocotools
+        cd models
+        cd research
+        protoc object_detection/protos/*.proto --python_out=.
+        pip install .
+        cd ..
+        cd ..
 fi
 
 
-pip install cython
-pip install pycocotools
 
-cd models
-cd research
-protoc object_detection/protos/*.proto --python_out=.
-pip install .
-cd ..
-cd ..
 
 cleanup
